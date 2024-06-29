d1d405ca2ff2cc74a95259ffba54a7d88fd28be8
akmojahid <texthouse7@gmail.com>
2024-06-29 21:50:39 +0600

    code added in html css js

--- html-css-js/script.js
+++ html-css-js/script.js
@@ -1,1 +1,5 @@
 console.log("This page is  under construction")
+
+const name = document.getElementById("name")
+const email = document.getElementById("email")
+const pass = document.getElementById("password")
