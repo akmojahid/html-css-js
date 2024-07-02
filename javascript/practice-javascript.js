ccbccce039c362aa832b716c890326b2e7cb7b94
akmojahid <texthouse7@gmail.com>
2024-07-02 11:41:51 +0600

    update practice-javascript.js

--- javascript/practice-javascript.js
+++ javascript/practice-javascript.js
@@ -3,3 +3,16 @@ let arr2 = ['x','y','z']
 
 let newArr = arr.concat(arr2)
 console.log(newArr)
+
+//reduce function
+let sum = arr.reduce(function (sum, cv){
+  return sum += cv;
+})
+console.log(sum)
+
+function fuc(a, b) {
+  return [a, b]
+}
+
+let ar = arr.reduce(fuc)
+console.log(ar)
