a999acce1b84894c4609c9dabf509ac83ede798c
akmojahid <texthouse7@gmail.com>
2024-07-05 20:27:28 +0600

    update dsa.js

--- javascript/dsa.js
+++ javascript/dsa.js
@@ -5,7 +5,10 @@ const obg =  {
   name: 'Jonah miha',
   profession: 'Senior Developer',
   age : 34
-  active: false
+  active: false,
+  user: function () {
+    return `My name is ${this.name}, I am a ${this.profession}`
+  }
 } 
 
-console.log(obg)
+console.log(obg.user())
